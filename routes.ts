b60ac3a8f201d7a206164dd54e612d342ab6e381
Eujin Ong <62314480+euj1n0ng@users.noreply.github.com>
2021-07-18 03:57:53 +0700

    Added caching functionality to all endpoints

--- routes.ts
+++ routes.ts
@@ -3,8 +3,12 @@ import { graphqlHTTP } from 'express-graphql';
 import { schema } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
 import { rateController } from './rest/controllers/rate-controller';
+import apicache from 'apicache';
 
 export const router = express.Router();
+
+let cache = apicache.middleware;
+router.use(cache('1 hour', (_: any, res: any) => res.statusCode === 200));
 
 router.use('/graphql', graphqlHTTP({
   schema,
